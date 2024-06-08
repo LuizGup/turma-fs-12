56e7e9506c5553d61a8b9b444c92fdd35972c750
LuizGup <117296493+LuizGup@users.noreply.github.com>
2024-06-08 17:02:41 -0300

    ..

---
+++ 7-react/aula-react/src/services/usuarios.ts
@@ -0,0 +1,14 @@
+import api from '../utils/api'
+
+const usuariosService = {
+listarUsuarios: async () => {
+const response = await api.get("usuarios");
+
+return response.data;
+},
+};
+
+
+export default{
+    listarUsuarios,
+;}
